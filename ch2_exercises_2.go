bebfcfc0b05cf6237558b7fa40c9dcef37a90a39
Rafael Guedes <rafaelguedes@gmail.com>
2024-09-10 15:41:10 -0300

    exercicios do capitulo 1 e 2

---
+++ ch2_exercises_2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const value = 10
+
+func main() {
+	var i int = value
+	var f float64 = value
+	fmt.Println("Os valores de i e f são:", i, f)
+}
---
+++ ch2_exercises_3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var b byte = 255
+	var smallI int32 = 2147483647
+	var bigI uint64 = 18446744073709551615
+	b += 1
+	smallI += 1
+	bigI += 1
+	fmt.Println("Os valores de b, smallI e bigI são:", b, smallI, bigI)
+}
